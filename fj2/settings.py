701cb96db0c88b0f374adc43fa22630571a20362
Cole Kubossek <Cole.Kubossek@gmail.com>
2021-01-22 11:18:01 -0800

    added whitenoise

--- fj2/settings.py
+++ fj2/settings.py
@@ -61,6 +61,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
